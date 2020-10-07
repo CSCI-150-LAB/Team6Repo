6eca04aa71b235fcab12d6dbbd6af1ac2efb8f5d
Alexander Her <35358559+AlexanderKHer@users.noreply.github.com>
2020-10-06 21:40:39 -0700

    admin added and installed new dependencies
    - check package.json file to view dependency
    - fixed fooditems schema to include userID
    - added admin router
    - added admin into server

--- Backend/models/FoodItems.js
+++ Backend/models/FoodItems.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const foodItemSchema = new Schema({
-  username: { type: String, required: true },
+  name: { type: String, required: true },
   description: { type: String, required: true },
   ethnicity: {type: String, required: false},
   cookingTime:{type: Number, required:true},
   date: { type: Date, required: true },
+  userID: { type: Schema.Types.ObjectId, ref: 'sellerSchema'},
   img: 
     { 
         data: Buffer, 
