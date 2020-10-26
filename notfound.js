9f31996f507118033644fe629985ff04d07e6e24
korybrooks <70187312+korybrooks@users.noreply.github.com>
2020-10-26 09:04:43 -0700

    Add files via upload

---
+++ notfound.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class notfound extends Component {
+    render() {
+        return (
+            <h3>Page Not Found</h3>
+        );
+    }
+}
