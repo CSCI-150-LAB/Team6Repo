4ab06b4bf5dd058582913ac28c1c6fbe9196a91d
korybrooks <70187312+korybrooks@users.noreply.github.com>
2020-10-26 09:05:25 -0700

    Delete notfound.js

--- notfound.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default class notfound extends Component {
-    render() {
-        return (
-            <h3>Page Not Found</h3>
-        );
-    }
-}
