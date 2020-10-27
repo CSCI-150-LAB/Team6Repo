7057ca0a72339d7093f8f28c3e15e066447676e5
korybrooks <70187312+korybrooks@users.noreply.github.com>
2020-10-27 15:46:26 -0700

    Add files via upload
    about us page

---
+++ ourstory.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./ourstory.css"
+
+export default class ourstory extends Component {
+    render() {
+        return (
+            <div>
+                <div class="b"></div>
+                <h3 class = "heading" style={{ color: '#0c9e06' }}>Who Are We?</h3>
+                <div class="a text-center text-secondary">Welcome to Authentic Chef, your number one source for authentic 
+                meals. We're dedicated to giving you the very best of food that is created by home chefs, with a 
+                focus on dependability, customer service, and uniqueness. Founded in 2020 by Koryale Brooks, Narendra Mannan,
+                 Randy Saetern, Alexander Her, and Megan Tamiyasu, Authentic Chef has come a long way from its beginnings at home for
+                 a class project. When we first started out, our passion for providing people with the best 
+                authentic food one could recieve at home from places that originate from all over the world
+                drove us to create a user friendly application, and gave us the impetus to turn hard work 
+                and inspiration into to a booming online store. We now serve customers all over the world, and are thrilled to be a 
+                part of the exciting and convenient wing the food industry.
+
+                We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or 
+                comments, please don't hesitate to contact us. </div>
+            </div>
+        );
+    }
+}
