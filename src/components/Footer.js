3f1d40913f7c1666635fdbc44340ccb1f7511de3
Narendra Mannan <53171217+narendramannan@users.noreply.github.com>
2020-11-05 15:39:41 -0800

    Updated heading and text color
    updated the footer heading and text color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import "./Footer.css"
 const Footer = () => {
     return(
         <div className="main-footer">
+            <br />
             <div className="container">
                 <div className="form-group">
                     <label>Enter your email to recieve updates </label>
@@ -15,7 +16,7 @@ const Footer = () => {
                 <div className="row">
                     {/* column1 */}
                     <div className="col">
-                        <h4>Authentic Cook</h4>
+                        <h4>Authentic Chef</h4>
                         <ul className="list-unstyled"> 
                         “One cannot think well, love well, sleep well, 
                         if one has not dined well.” -Virginia Woolf
@@ -24,17 +25,17 @@ const Footer = () => {
                     {/* column2 */}
                     <div className="col">
                         <ul className="list-unstyled">
-                            <li><a href="/" style={{ color: 'black' }} >Our Story</a></li>
-                            <li><a href="/" style={{ color: 'black' }}>How it Works</a></li>
-                            <li><a href="/" style={{ color: 'black' }}>Privacy Policy</a></li>
+                            <li><a href="/">Our Story</a></li>
+                            <li><a href="/">How it Works</a></li>
+                            <li><a href="/">Privacy Policy</a></li>
                         </ul>
                     </div>
                     {/* column3 */}
                     <div className="col">
                         <ul className="list-unstyled">
-                            <li><a href="/" style={{ color: 'black' }}>Hosts</a></li>
-                            <li><a href="/" style={{ color: 'black' }}>FAQ's</a></li>
-                            <li><a href="/" style={{ color: 'black' }}>Refunds and Cancellation</a></li>
+                            <li><a href="/">Hosts</a></li>
+                            <li><a href="/">FAQ's</a></li>
+                            <li><a href="/">Refunds and Cancellation</a></li>
                         </ul>
                     </div>
                 </div>
