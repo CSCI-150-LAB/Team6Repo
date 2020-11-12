a5de234abca84a3b886a6101aa0f29c3e228debe
Narendra Mannan <53171217+narendramannan@users.noreply.github.com>
2020-11-12 13:12:05 -0800

    updated registeration form
    UI changes to registration form

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,56 +1,54 @@
-import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
+import React, {Component} from "react";
+import {Link, withRouter} from "react-router-dom";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { registerUser } from "../actions/authActions";
+import {connect} from "react-redux";
+import {registerUser} from "../actions/authActions";
 import classnames from "classnames";
-
+import {Form, FormGroup, Label, Input} from 'reactstrap';
+import Button from 'react-bootstrap/Button'
 class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.submit = this.onSubmit.bind(this); 
+    constructor(props) {
+        super(props);
+        this.submit = this.onSubmit.bind(this);
 
-    this.state = {
-      name: "",
-      email: "",
-      password: "",
-      password2: "",
-      role: "", 
-      errors: {}
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+            password2: "",
+            role: "",
+            errors: {}
+        }
     }
-  }
-  componentDidMount() {
-    // If logged in and user navigates to Register page, should redirect them to dashboard
-    if (this.props.auth.isAuthenticated) {
-      this.props.history.push("/landingPage");
+    componentDidMount() { // If logged in and user navigates to Register page, should redirect them to dashboard
+        if (this.props.auth.isAuthenticated) {
+            this.props.history.push("/landingPage");
+        }
     }
-  }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({
-        errors: nextProps.errors
-      });
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({errors: nextProps.errors});
+        }
     }
-  }
 
-onChange = e => {
-    this.setState({ [e.target.id]: e.target.value })
-}
+    onChange = e => {
+        this.setState({[e.target.id]: e.target.value})
+    }
 
 
-onSubmit = e => {
-    e.preventDefault();
-const newUser = {
-      name: this.state.name,
-      email: this.state.email,
-      password: this.state.password,
-      password2: this.state.password2,
-      role: this.state.role
-    };
+    onSubmit = e => {
+        e.preventDefault();
+        const newUser = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            password2: this.state.password2,
+            role: this.state.role
+        };
 
-this.props.registerUser(newUser, this.props.history); 
-/*
+        this.props.registerUser(newUser, this.props.history);
+        /*
 axios.post('http://localhost:5000/routes/users/register', newUser)
     .then(res=>{
       console.log(res.data)
@@ -62,137 +60,157 @@ axios.post('http://localhost:5000/routes/users/register', newUser)
       }
     })
     */
-console.log(newUser);
+        console.log(newUser);
 
-  };
+    };
 
 
-render() {
-    const { errors } = this.state;
-return (
-      <div className="container">
-        <div className="row">
-          <div className="col s8 offset-s2">
-            <Link to="/home" className="btn-flat waves-effect">
-              <i className="material-icons left"></i> Back to
-              home page
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Register</b> below
-              </h4>
-              <p className="grey-text text-darken-1">
-                Already have an account? <Link to="/login">Log in</Link>
-              </p>
+    render() {
+        const {errors} = this.state;
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col s8 offset-s2">
+                        <Link to="/home" className="btn-flat waves-effect">
+                        <i className="material-icons left">Go back to Home</i>      
+                        </Link>
+                            <h2 className="text-center">
+                                <b>Register </b>
+                                 with us
+                            </h2>
+                        <form className="sign-up-form" noValidate
+                            onSubmit={
+                                this.onSubmit
+                        }>
+                            <FormGroup>
+                                <label htmlFor="name">Name</label>
+                                <br />
+                                <input onChange={
+                                        this.onChange
+                                    }
+                                    value={
+                                        this.state.name
+                                    }
+                                    error={
+                                        errors.name
+                                    }
+                                    id="name"
+                                    type="text"
+                                    placeholder="Your Full Name"
+                                    className={
+                                        classnames("", {invalid: errors.name})
+                                    }/>
+                                <span className="red-text">
+                                    {
+                                    errors.name
+                                }</span>
+                            </FormGroup>
+                            <FormGroup>
+                                <label htmlFor="email">Email</label>
+                                <br />
+                                <input onChange={
+                                        this.onChange
+                                    }
+                                    value={
+                                        this.state.email
+                                    }
+                                    error={
+                                        errors.email
+                                    }
+                                    id="email"
+                                    type="email"
+                                    className={
+                                        classnames("", {invalid: errors.email})
+                                    }/>
+                            </FormGroup>
+
+
+                            <FormGroup>
+                                <label htmlFor="password">Password</label>
+                                <br />
+                                <input onChange={
+                                        this.onChange
+                                    }
+                                    value={
+                                        this.state.password
+                                    }
+                                    error={
+                                        errors.password
+                                    }
+                                    id="password"
+                                    type="password"
+                                    className={
+                                        classnames("", {invalid: errors.password})
+                                    }/>
+                                <span className="red-text">
+                                    {
+                                    errors.name
+                                }</span>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <label htmlFor="password2">Confirm Password</label>
+                                <br />
+                                <input onChange={
+                                        this.onChange
+                                    }
+                                    value={
+                                        this.state.password2
+                                    }
+                                    error={
+                                        errors.password2
+                                    }
+                                    id="password2"
+                                    type="password"/>
+                                <span className="red-text">
+                                    {
+                                    errors.name
+                                }</span>
+                            </FormGroup>
+
+
+                            <FormGroup>
+                                <label htmlFor="role">Seller or Buyer</label>
+                                <br />
+                                <input onChange={
+                                        this.onChange
+                                    }
+                                    value={
+                                        this.state.role
+                                    }
+                                    error={
+                                        errors.role
+                                    }
+                                    id="role"
+                                    type="role"
+                                    placeholder="Enter buyer or seller"
+                                    className={
+                                        classnames("", {invalid: errors.role})
+                                    }/>
+                                <span className="red-text">
+                                    {
+                                    errors.name
+                                }</span>
+                            </FormGroup>
+
+                            <Button className="btn-block" variant="outline-success" type="submit">
+                                {'Sign Up'} </Button>
+                            <div className="text-center">
+                                Already have an account? <Link to="/login"> Log in</Link>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
             </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.name}
-                  error={errors.name}
-                  id="name"
-                  type="text"
-                  className={classnames("", {
-                    invalid: errors.name
-                  })}
-                />
-                <label htmlFor="name">Name</label>
-                <span className="red-text">{errors.name}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                  className={classnames("", {
-                    invalid: errors.email
-                  })}
-                />
-                <label htmlFor="email">Email</label>
-              </div>
-
-              
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password
-                  })}
-                />
-                <label htmlFor="password">Password</label>
-                <span className="red-text">{errors.name}</span>
-              </div>
-
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password2}
-                  error={errors.password2}
-                  id="password2"
-                  type="password"
-                />
-                <label htmlFor="password2">Confirm Password</label>
-                <span className="red-text">{errors.name}</span>
-              </div>
-
-
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.role}
-                  error={errors.role}
-                  id="role"
-                  type="role"
-                  className={classnames("", {
-                    invalid: errors.role
-                  })}
-                />
-                <label htmlFor="role">please enter "seller" or "buyer" nothing else pls</label>
-                <span className="red-text">{errors.name}</span>
-              </div>
-
-
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Sign up
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-Register.propTypes = {
-  registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
+        );
+    }
+} Register.propTypes = {
+    registerUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
 };
 
-const mapStateToProps = state => ({
-  auth: state.auth,
-  errors: state.errors
-});
+const mapStateToProps = state => ({auth: state.auth, errors: state.errors});
 
-export default connect(
-  mapStateToProps,
-  { registerUser }
-)(withRouter(Register));
+export default connect(mapStateToProps, {registerUser})(withRouter(Register));
