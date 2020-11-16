4b9deafbda0c91e401ccea229b740a3980c53e51
Randy Saetern <53891483+randysaetern@users.noreply.github.com>
2020-11-15 17:24:28 -0800

    Food Search
    Food Search has been implemented. It currently queries the data from mongoDB using indexes to specify search mechanism. Currently, The interface for pictures are missing and it will only display the text parts of the food item.

--- src/components/food_search_component.js
+++ src/components/food_search_component.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import ".././App.css";
+
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+
+import Food1 from "../imagesForMain/cards/1.jpeg";
+import { TableCell } from 'admin-bro';
+
+const Fooditem = props =>(
+  <tr>
+    <td>{props.food.chefname}</td>
+    <td>{props.food.foodname}</td>
+    <td>{props.food.description}</td>
+    <td>{props.food.price}</td>
+    <img src = {"/uploads/" + props.food.chefname + "_" + props.food.foodname + props.food._id} alt = ""/>
+    <td>
+      <Link to={"/edit/"+props.exercise._id}>View</Link>
+    </td>
+  </tr>
+)
 
 export default class CreateSearchQuery  extends Component{
     
@@ -21,24 +45,46 @@ export default class CreateSearchQuery  extends Component{
         search: e.target.value
         })
     }
+    /*
+    componentDidMount(){
+      axios.get("http://localhost:5000/searchfood/name",{params:{"description":"fried rice"}}).then(response =>{
+          if(response.data.length >= 0){
+            this.setState({fooditems: response.data})
+          }
+          else{
+            console.log("NOTHING");
+          }
+      });
+    }
+    */
+    debugpurp = e =>{
+      console.log(this.state.fooditems);
+    }
+
+    foodList() {
+      return this.state.fooditems.map(currentfood => {
+        return <Fooditem food={currentfood} key={currentfood._id}/>;
+      })
+    }
     onSubmit(e){
         e.preventDefault();
 
         const foodsearch = {description : this.state.search};
         console.log(foodsearch);
         axios.get("http://localhost:5000/searchfood/name",{params:foodsearch}).then(response =>{
-          this.setState({fooditems: response.data}); 
-          console.log(response.data);
+          if(response.data.length >= 0){
+            this.setState({fooditems: response.data})
+          }
+          else{
+            console.log("NOTHING");
+          }
       });
-
     }
 
     render() {
         return (
           <div>
-            <h3>Food Search
-                
-            </h3>
+            <h3>Food Search</h3>
             <form onSubmit={this.onSubmit}>
               <div className="form-group"> 
                 <label>search: </label>
@@ -54,12 +100,25 @@ export default class CreateSearchQuery  extends Component{
                 " className="btn btn-primary" />
               </div>
             </form>
+            <button onClick= {this.debugpurp}>testing</button>
             <div>
-              <h3> Name of Chef </h3>
-              <p>{this.state.fooditems.name}</p>
-              <h3> Item Description: {this.state.fooditems.description} </h3>
-              <h3> PRICE IS : {this.state.fooditems.price} </h3>
-            </div>
+        <h3>Searched Results:</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Chefname</th>
+              <th>Food Name</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Image</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.foodList() }
+          </tbody>
+        </table>
+      </div>
           </div>
         )
       }
