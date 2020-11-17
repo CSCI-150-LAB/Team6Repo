8ca56ef37363e4a84504797a305f166927f78787
Randy Saetern <53891483+randysaetern@users.noreply.github.com>
2020-11-17 12:18:55 -0800

    Better search

--- Backend/routes/food_search.js
+++ Backend/routes/food_search.js
@@ -4,38 +4,7 @@ const mongoose = require("mongoose")
 const FoodItem= require("../models/FoodItems");
 
 router.get("/", (req, res, next) => {
-  FoodItem.find()
-    .select("name price productImage")
-    .exec()
-    .then(docs => {
-      const response = {
-        count: docs.length,
-        fooditems: docs.map(doc => {
-          return {
-            chefname: doc.chefname,
-            price: doc.price,
-            productImage: doc.productImage,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/search/" + doc._id
-            }
-          };
-        })
-      };
-      //   if (docs.length >= 0) {
-      res.status(200).json(response);
-      //   } else {
-      //       res.status(404).json({
-      //           message: 'No entries found'
-      //       });
-      //   }
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
+  FoodItem.find().then(fooditem => res.json(fooditem)).catch(err => res.status(420).json(err));
 });
 
 router.get("/name", (req, res,next) => {
