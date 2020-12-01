3145762ae990d2a69c1ca62570de7be03a2636fa
korybrooks <koryale15@mail.fresnostate.edu>
2020-12-01 11:29:31 -0800

    Merging Frontend
    New frontend features

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import "./Footer.css"
-//import styled from 'styled-components'
+import "./Footer.css";
+import styled from 'styled-components'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+
+
 
 const Footer = () => {
     return(
         <div className="main-footer">
-            <br />
             <div className="container">
                 <div className="form-group">
                     <label>Enter your email to recieve updates </label>
                     <input type="email" className="form-control" placeholder="Enter email" />
                 </div>
-                <a href="/#" className=" btn-outline-dark btn-lg" role="button" aria-pressed="true">SIGN UP</a>
+                <a href="#" class=" btn-outline-dark btn-lg" role="button" aria-pressed="true">SIGN UP</a>
                 <hr/>
                 <div className="row">
                     {/* column1 */}
                     <div className="col">
-                        <h4>Authentic Chef</h4>
+                        <h4>Authentic Cook</h4>
                         <ul className="list-unstyled"> 
                         “One cannot think well, love well, sleep well, 
                         if one has not dined well.” -Virginia Woolf
@@ -25,17 +28,16 @@ const Footer = () => {
                     {/* column2 */}
                     <div className="col">
                         <ul className="list-unstyled">
-                            <li><a href="/">Our Story</a></li>
-                            <li><a href="/">How it Works</a></li>
-                            <li><a href="/">Privacy Policy</a></li>
+                            <li><a href="ourstory" style={{ color: 'black' }} >Our Story</a></li>
+                            <li><a href="howitworks" style={{ color: 'black' }}>How it Works</a></li>
+                            <li><a href="privacypolicy" style={{ color: 'black' }}>Privacy Policy</a></li>
                         </ul>
                     </div>
                     {/* column3 */}
                     <div className="col">
                         <ul className="list-unstyled">
-                            <li><a href="/">Hosts</a></li>
-                            <li><a href="/">FAQ's</a></li>
-                            <li><a href="/">Refunds and Cancellation</a></li>
+                            <li><a href="contactus" style={{ color: 'black' }}>Contact Us</a></li>
+                            <li><a href="rulespage" style={{ color: 'black' }}>Do's and Don'ts</a></li>
                         </ul>
                     </div>
                 </div>
@@ -45,9 +47,11 @@ const Footer = () => {
                         &copy;{new Date().getFullYear()} Authentic Chef INC | All rights reserved
                     </p>
                 </div>
+
+
             </div>
         </div>
     )
+
 }
 export default Footer;
-
---
+++ src/components/privacypolicy.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import "./privacypolicy.css"
+
+export default class privacypolicy extends Component {
+    render() {
+        return (
+            <div id="privacypolicy">
+            <h3>Privacy Policy</h3>
+            <p class="text-center">Authentic Chef provides an online platform through its website https://www.AuthenticChef.com, that connects 
+            customers to purchase food from home cooks by either pick up or delivery (collectively, 
+            the “Services”). These services, as mentioned above, are provided through the website 
+            https://www.AuthenticChef.com ("the Website") to you, the user, subject to your compliance with all the terms,
+             conditions, and notices contained or referred herein (the "Privacy Policy"), as well as any other written 
+             agreement between us and you. In addition, when using particular services or materials on this Webite, users 
+             shall be subject to any posted rules applicable to such services or materials that may contain terms and 
+             conditions in addition to those mentioned herein. All such guidelines or rules are hereby incorporated by 
+             reference into these terms.
+
+            Authentic Chef may be used interchangeably and shall be treated as one for all references below.</p>
+            <h6> Gathering of Personally-Identifiable Information</h6>
+            <p class="text-xl-left">Certain visitors to the Website choose to interact with Authentic Chef in ways that 
+            require Authentic Chef to gather personally-identifying information. The amount and type of information that 
+            Authentic Chef gathers depends on the nature of the interaction. For example, we ask visitors who sign up at AuthenticChef.com 
+            to provide a email address and password. The users engaging in transactions with 
+            Authentic Chef are asked to provide additional information, including as necessary the personal and financial 
+            information required to process those transactions. In each case, Authentic Chef collects such information only 
+            insofar as is necessary or appropriate to fulfill the purpose of the visitor’s interaction with Authentic Chef. 
+            Authentic Chef does not disclose personally-identifiable information other than as described below. Users can 
+            always refuse to supply personally-identifiable information, with the caveat that it may prevent them from 
+            engaging in certain website-related activities.</p>
+            <h6>Protection of Certain Personally-Identifiable Information</h6>
+            <p class="text-xl-left">Authentic Chef may disclose potentially personally-identifiable and personally-identifying information 
+                only to those of its employees, contractors and affiliated organizations that (i) need to know such 
+                information in order to process it on Authentic Chef’s behalf or to provide services available at the 
+                Website, and (ii) have agreed not to disclose it to others. Some of those employees, contractors and 
+                affiliated organizations may be located outside of your home country by using the Website, you consent to 
+                the transfer of such information to them. In addition, in some cases we may choose to buy or sell assets. 
+                In these types of transactions, user information is typically one of the business assets that is transferred. 
+                Moreover, if Authentic Chef or substantially all of its assets were acquired, or in the unlikely event that 
+                Authentic Chef goes out of business or enters bankruptcy, user information would be one of the assets that is
+                 transferred or acquired by a third party. You acknowledge that such transfers may occur, and that any 
+                 acquirer of Authentic Chef may continue to use your personal and non-personal information only as set forth 
+                 in this policy. Otherwise, Authentic Chef will not rent or sell potentially personally-identifying and 
+                 personally-identifying information to anyone.
+
+                Authentic Chef may share potentially personally-identifiable and personally-identifiable information only 
+                when required to do so by law, regulation, legal process or Government request and if webelieve in good 
+                faith that disclosure is reasonably necessary to protect the property, rights or personal safety of 
+                Authentic Chef, its users or the general public at large. If you are a registered user of the Website and 
+                have supplied your contact details, we shall use your contact details for the purpose of informing you 
+                regarding new features, to solicit your feedback, or just keep you up to date with what’s going on with 
+                us and our products. We primarily use our various product blogs to communicate this type of information. 
+                If you send us a request (for example via a support email or via one of our feedback mechanisms), we 
+                reserve the right to publish it in order to help us clarify or respond to your request or to help us 
+                support other users. Authentic Chef takes all measures reasonably necessary to protect against the 
+                unauthorized access, use, alteration or destruction of potentially personally-identifying and 
+                personally-identifying information.
+
+                You should also be aware that if you submit information to "chat rooms", "forums" or "message boards" such 
+                information becomes public information, meaning that you lose any privacy rights you might have with 
+                regards to that information. Such disclosures may also increase your chances of receiving unwanted 
+                communications.
+
+                Any information you share on the Website will be subject at all times to the Data Privacy Laws, 
+                Intellectual Property Laws and publishing laws that will be in effect at the material time in India.</p>
+            <h6>Security</h6>
+            <p class="text-xl-left">All non-personally-identifiable information, potentially personally-identifiable and 
+            personally identifiable-information described above is stored on restricted database servers.</p>
+            <h6>Choice/Opt-out</h6>
+            <p class="text-xl-left">If we ever send you information by e-mail concerning new products, services or 
+            information that you did not expressly request, we will provide you with an e-mail address by which you may 
+            request no further notices.</p>
+            <h6>Address Book Data</h6>
+            <p class="text-xl-left">Any external address book data (email contacts, etc.) that a user voluntarily gives 
+            Authentic Chef access to will only be used for the described feature (looking up friends, etc.), and will not 
+            be stored or repurposed.</p>
+            <h6>Privacy Policy Changes</h6>
+            <p class="text-xl-left">Authentic Chef reserves the right to change this Privacy Policy anytime, and at our 
+            sole discretion. So you are requested to revisit this page frequently. In case of any material changes we 
+            will make reasonable effort to post a clear notice of the same.In all cases, use of information we collect 
+            now is subject to the Privacy Policy in effect at the time such information is collected. Changes to this 
+            Privacy Policy are effective as of the stated “Last Revised” and your continued use of the Service on or the 
+            Last Revised date will constitute acceptance of, and agreement to be bound by, those changes.</p>
+            </div>
+            
+        );
+    }
+}
