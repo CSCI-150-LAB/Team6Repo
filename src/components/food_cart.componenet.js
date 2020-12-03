03ae6122f1f8ded6cb180c51c60e65b9914ce2df
Narendra Mannan <53171217+narendramannan@users.noreply.github.com>
2020-12-03 03:13:14 -0800

    Shopping Cart updated
    Updated design again because of merge issue

--- src/components/food_cart.componenet.js
+++ src/components/food_cart.componenet.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
 import ".././App.css";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { PayPalButton } from "react-paypal-button-v2";
+import {PayPalButton} from "react-paypal-button-v2"; 
+import Button from "react-bootstrap/Button";
 import "fontsource-roboto";
-const FoodItems = (props) => (
+
+const FoodItems = props => (
   <tr>
     <th scope="row" className="border-0">
       <div className="p-2">
@@ -19,14 +21,12 @@ const FoodItems = (props) => (
     <td className="border-0 align-middle">{props.fooditem.chefname}</td>
     <td className="border-0 align-middle">${props.fooditem.price}</td>
     <td className="border-0 align-middle">
-    <Button onClick={() => { props.deleteFoodItem(props.fooditem._id) }}>delete</Button>
+      <Button onClick={() => { props.deleteFoodItem(props.fooditem._id) }}>delete</Button>
     </td>
   </tr>
-);
+)
 
-class foodCart extends Component {
-  constructor(props) {
-    super(props);
+class foodCart  extends Component{
 
     constructor(props){ 
         super(props);
@@ -40,27 +40,23 @@ class foodCart extends Component {
         }
     }
 
-  componentDidMount() {
-    console.log(this.props.auth);
-    axios
-      .get(
-        "http://localhost:5000/foodcart/getcart/" + this.props.auth.user.id,
-        { username: this.props.auth.user.name }
-      )
-      .then((response) => {
-        this.setState({ userCart: response.data });
-        this.setState({ fooditems: response.data.foodItems });
+    
+    componentDidMount(){
+      console.log(this.props.auth);
+      axios.get("http://localhost:5000/foodcart/getcart/"+ this.props.auth.user.id,{username:this.props.auth.user.name}).then(response =>{
+        this.setState({userCart:response.data});
+        this.setState({fooditems:response.data.foodItems});
         console.log(response.data);
-        if (response.data.foodItems.length > 0) {
+        if(response.data.foodItems.length>0){
           var temp = 0;
-          for (var i = 0; i < response.data.foodItems.length; i++) {
+          for(var i = 0; i < response.data.foodItems.length; i++){
             temp = response.data.foodItems[i].price + temp;
           }
-
-          var tax = temp * 0.07;
-          tax = Math.floor(tax * 100) / 100;
-          this.setState({ tax: tax });
-          this.setState({ price: temp });
+         
+          var tax = (temp * 0.07);
+          tax = Math.floor(tax * 100) / 100; 
+          this.setState({tax:tax});
+          this.setState({price:temp});
         }
       });
     }
@@ -91,92 +87,87 @@ class foodCart extends Component {
       axios.delete("http://localhost:5000/foodcart/deletecart/"+ this.props.auth.user.id).then(() => { 
         console.log("cart has been deleted");
       });
-  };
+    }
   foodList() {
-    return this.state.fooditems.map((currentfood) => {
-      return (
-        <FoodItems
-          fooditem={currentfood}
-          key={currentfood._id}
-          deleteFoodItem={this.deleteFoodItem}
-        />
-      );
-    });
+    return this.state.fooditems.map(currentfood => {
+      return <FoodItems fooditem={currentfood} key={currentfood._id} deleteFoodItem = {this.deleteFoodItem}/>;
+    })
   }
 
-  refreshPage() {
-    window.location.reload(true);
+  refreshPage() { 
+    window.location.reload(true); 
   }
-
+ 
   render() {
     return (
       <div className="pb-5">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12 p-5 bg-white rounded shadow-sm mb-5">
-              <h3 className="text-uppercase">
-                {this.props.auth.user.name}'s cart
-              </h3>
-              <div className="table-responsive">
-                <table className="table">
-                  <thead>
-                    <tr>
-                      <th scope="col" className="border-0 bg-success">
-                        <div className="text-uppercase">Item</div>
-                      </th>
-                      <th scope="col" className="border-0 bg-success">
-                        <div className="text-uppercase">Chef</div>
-                      </th>
-                      {/* <th scope="col" className="border-0 bg-success">
-                        <div className="text-uppercase">Description </div>
-                      </th> */}
-                      <th scope="col" className="border-0 bg-success">
-                        <div className="text-uppercase">Price</div>
-                      </th>
-                      <th scope="col" className="border-0 bg-success">
-                        <div className="text-uppercase">Action</div>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>{this.foodList()}</tbody>
-                </table>
-              </div>
-              <h2>Price: ${this.state.price}</h2>
-              <h2>Tax: ${this.state.tax}</h2>
-              <h2>Total Price: ${this.state.price + this.state.tax}</h2>
-
-              <PayPalButton
-                amount={this.state.price + this.state.tax}
-                onSuccess={(details, data) => {
-                  alert(
-                    "Transaction completed by " + this.props.auth.user.name
-                  );
-                  this.deleteCart();
-                  this.refreshPage();
-                }}
-                options={{
-                  clientId:
-                    "AfX37SWBIHQyn3WOrVxzbNg3fvq5kAnUpEqhG7CHqUvc8Mla5po49GJWuLblE1X1Ybs9hIpHF3JjVulX",
-                  merchantId: "698584Y22ED8E",
-                  currency: "USD",
-                }}
-              />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 p-5 bg-white rounded shadow-sm mb-5">
+            <h3 className="text-uppercase">
+              {this.props.auth.user.name}'s cart
+            </h3>
+            <div className="table-responsive">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col" className="border-0 bg-success">
+                      <div className="text-uppercase">Item</div>
+                    </th>
+                    <th scope="col" className="border-0 bg-success">
+                      <div className="text-uppercase">Chef</div>
+                    </th>
+                    {/* <th scope="col" className="border-0 bg-success">
+                      <div className="text-uppercase">Description </div>
+                    </th> */}
+                    <th scope="col" className="border-0 bg-success">
+                      <div className="text-uppercase">Price</div>
+                    </th>
+                    <th scope="col" className="border-0 bg-success">
+                      <div className="text-uppercase">Action</div>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>{this.foodList()}</tbody>
+              </table>
             </div>
-          </div>
+            <h2>Price: ${this.state.price}</h2>
+            <h2>Tax: ${this.state.tax}</h2>
+            <h2>Total Price: ${this.state.price + this.state.tax}</h2>
 
-          <div class="row py-5 p-4 bg-white rounded shadow-sm">
-            <div class="col-lg-6"></div>
+            <PayPalButton
+              amount={this.state.price + this.state.tax}
+              onSuccess={(details, data) => {
+                alert(
+                  "Transaction completed by " + this.props.auth.user.name
+                );
+                this.deleteCart();
+                this.refreshPage();
+              }}
+              options={{
+                clientId:
+                  "AfX37SWBIHQyn3WOrVxzbNg3fvq5kAnUpEqhG7CHqUvc8Mla5po49GJWuLblE1X1Ybs9hIpHF3JjVulX",
+                merchantId: "698584Y22ED8E",
+                currency: "USD",
+              }}
+            />
           </div>
         </div>
+
+        <div class="row py-5 p-4 bg-white rounded shadow-sm">
+          <div class="col-lg-6"></div>
+        </div>
       </div>
-    );
+    </div>
+  );
   }
 }
 
 foodCart.propTypes = {
-  auth: PropTypes.object.isRequired,
-};
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-});
-export default connect(mapStateToProps)(foodCart);
+    auth: PropTypes.object.isRequired
+  };
+  const mapStateToProps = state => ({
+    auth: state.auth
+  });
+  export default connect(
+    mapStateToProps)(foodCart);
